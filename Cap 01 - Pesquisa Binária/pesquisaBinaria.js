e7c688a4d68a6b6b7a2e61c5005ee8f9c3ca0cfb
Mariano Carlos Silva <76002553+Mariano-SI@users.noreply.github.com>
2022-07-18 08:44:51 -0300

    Add files via upload

---
+++ Cap 01 - Pesquisa Binária/pesquisaBinaria.js
@@ -0,0 +1,27 @@
+function pesquisaBinaria(lista, item) {
+    var baixo = 0;
+    var alto = lista.length -1;
+    while (baixo <= alto) {
+        var meio = Math.ceil((baixo + alto)/2);
+        var chute = lista[meio];
+        if (chute == item) {
+            console.log("O index de " + item + " é " + meio);
+            return meio;
+        }else if (chute > item) {
+            alto = meio - 1;
+        }else{
+            baixo = meio + 1;
+        }
+    }  
+    console.log(meio)
+  }
+  
+
+let meuArray = new Array();
+for (let index = 0; index < 1000 ; index++) {
+    meuArray[index] = index*3;
+}
+
+console.log(meuArray)
+let teste = pesquisaBinaria(meuArray, 267);
+console.log(meuArray[teste]);
